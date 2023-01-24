760d26e5ea309eb43c5b47110b45de7c0080512e
Ankit Kumar <ankitkr0696@gmail.com>
2023-01-24 11:39:04 +0530

    max upload size to 10gb

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,22 +1,22 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
   // 'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
   {
     name: "strapi::body",
     config: {
-      formLimit: "1024mb", // modify form body
+      formLimit: "10gb", // modify form body
       jsonLimit: "256mb", // modify JSON body
       textLimit: "256mb", // modify text body
       formidable: {
-        maxFileSize: 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 10 * 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
       },
     },
   },
--- config/plugin.js
+++ config/plugin.js
@@ -1,14 +1,14 @@
 // path: ./config/plugins.js
 
 module.exports = {
-    // ...
-    upload: {
-      config: {
-        providerOptions: {
-            // 1gb in bytes
-          sizeLimit: 1024 * 1024 * 1024,
-        }
-      }
-    }
-  };
+  // ...
+  upload: {
+    config: {
+      providerOptions: {
+        //10* 1gb in bytes
+        sizeLimit: 10 * 1024 * 1024 * 1024,
+      },
+    },
+  },
+};
   
